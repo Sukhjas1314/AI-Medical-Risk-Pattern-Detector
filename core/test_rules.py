123f6c146d4ad3a4414d6c24fe02972176173082
Sukhmanpreet Singh <ssbakshi2050@gmail.com>
2026-02-08 23:26:09 +0530

    Update test_rules.py

--- core/test_rules.py
+++ core/test_rules.py
@@ -1,5 +1,6 @@
 import json
 from rules_engine import check_care_rules
+from risk_scoring import calculate_risk_score
 
 with open("data/patients.json") as f:
     data = json.load(f)
@@ -12,5 +13,9 @@ timeline = [e for e in data if e["patient_id"] == patient_id]
 timeline.sort(key = lambda x: x["timestamp"])
 
 violations = check_care_rules(timeline)
+score,level,explainations = calculate_risk_score(violations)
 
 print("Violations : ",violations)
+print("Risk score : ",score)
+print("Risk Level : ",level)
+print("Explainations : ",explainations)
